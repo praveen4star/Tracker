cd18632c348d04285b9a7e5f5e9ccfbe90f3bcdb
Tarun Pandey <you@example.com>
2023-02-06 22:13:10 +0530

    route setup

--- src/App.js
+++ src/App.js
@@ -10,26 +10,18 @@ import Home from 'pages/Home';
 
 function App() {
   return (
-    <div className="main">
-      <Home />
-      {/* <SideBar /> */}
-     
-       {/* <div className='main-page'>
-    
-          <Routes>
-            <Route path='/dashboard' element={<Dashboard />} />
-            <Route path='/dailyroutinue' element={<DailyRoutinue />} />
-            <Route path='/stats' element={<Stats />} />
-            <Route path='/profile' element={<Profile />} />
-            <Route path='/logout' element={<Logout />} />
-            <Route path='/' element={<Home />} /> 
-            <Route path="*" element={<Navigate to="/dashboard" replace />}/>
-          </Routes>
-        </div> */}
-        
-    
-      
-    </div>
+  <div className="main">
+      <Routes>    
+        <Route path="/"  element={<Home />}/>
+          <Route path="/dashboard" element={<SideBar />}>
+              <Route index element={<Dashboard />} />
+              <Route path='dailyroutinue' element={<DailyRoutinue />} />
+              <Route path='stats' element={<Stats />} />
+              <Route path='profile' element={<Profile />} />
+              <Route path='logout' element={<Logout />} />
+          </Route>
+      </Routes>
+  </div>
   );
 }
 
