a32ec5dc6db1947d79568820a41898663ffbe848
Ashutosh Yadav <ashuydv4149@gmail.com>
2023-02-14 18:52:01 +0530

    final registration

--- src/pages/registration/index.js
+++ src/pages/registration/index.js
@@ -53,7 +53,8 @@ margin:1rem;
 padding: 1rem 6rem;
 border: 1px solid transparent;
 border-radius: 10px;
-box-shadow: rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px;
+box-shadow: rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px;
+
 `
 const FieldText = styled.label`
 margin: 4px;
